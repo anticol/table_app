61939961b45fabd8f75f3de152edd53821d328bf
David Horov <david.horov@primehammer.com>
2024-12-03 12:45:18 +0100

    feat: comments

--- src/hooks/useFetchUsers.tsx
+++ src/hooks/useFetchUsers.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { User } from "../types/User";
 
+// Fetch users from random data API
 const fetchUsers = async (size: number): Promise<User[]> => {
     const response = await axios.get(
         `https://random-data-api.com/api/users/random_user?size=${size}`
@@ -9,6 +10,7 @@ const fetchUsers = async (size: number): Promise<User[]> => {
     return response.data;
 };
 
+// Custom hook to fetch users 
 export const useUsers = (size: number) => {
     return useQuery<User[]>({
         queryKey: ["users", size], // Include size in the queryKey for caching
